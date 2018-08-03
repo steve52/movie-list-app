4790b3de6861aa7d9bccc0a6aa1e23e9c4273768
Stephen Giordano <steve1_nyc@yahoo.com>
2018-08-03 14:56:52 -0400

    update how we connect to mongodb

--- server.js
+++ server.js
@@ -11,21 +11,29 @@ let distDir = __dirname + "/client/build/";
 app.use(express.static(distDir));
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/MovieListdb');
+let promise = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/MovieListdb', {
+  useMongoClient: true
+});
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+promise.then(() => {
+  console.log('Connected to mongodb');
+
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+  let routes = require('./routes/movieListRoutes');
+  routes(app);
+
+  app.use(function(req, res, next) {
+    res.status(404).send(`${req.originalUrl} not found`);
+  })
+
+  app.listen(port);
+
+  console.log(`movie list RESTful API server started on: ${port}`);
+}, (err) => {
+  console.error('Failed to connect to mongodb', err);
+});
 
 
-
-var routes = require('./routes/movieListRoutes');
-routes(app);
-
-app.use(function(req, res, next) {
-  res.status(404).send(`${req.originalUrl} not found`);
-})
-
-app.listen(port);
-
-console.log(`movie list RESTful API server started on: ${port}`);
